4c8d85af01f3fec9ce096540a3234262e4005075
jawwadturabi <memonjawwad9@gmail.com>
2019-04-09 02:57:25 +0500

    j

--- index.ts
+++ index.ts
@@ -92,72 +92,142 @@ async function humidity(req, res) {
     })
 }
 
-function rain(req, res) {
-    res.send({
-
-        fulfillmentText: `Hello! I am your Weather Assistant`
-    })
-}
-
-function temp(req, res) {
-    var cityN_ame = req.body.queryResult.parameters.city
-    var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityN_ame}&units=metric&appid=${apiKey}`
-    if (!req.body.queryResult.parameters.city) {
-        res.send({
-
-            fulfillmentText: `Please enter the city name`
-        })
-
+async function rain(req, res) {
+    
+    console.log("context are: ",req.body.queryResult.outputContexts)
+    var cityName;
+    if (req.body.queryResult.parameters.city) {
+        cityName = req.body.queryResult.parameters.city
+    }
+    else if (req.body.queryResult.outputContexts[1].parameters.abccity) {
+        cityName = req.body.queryResult.outputContexts[1].parameters.abccity
     }
     else {
-
-        rp.get(url, function (err, _res, body) {
-            let weather = JSON.parse(body);
-            if (err) {
-                console.log('error:', err);
-            } else {
-                res.send({
-                    fulfillmentText: `The temperature in ${cityN_ame} is ${weather.main.temp}°C !`
-                })
-            }
-        }
-        )
+        res.send({
+            fulfillmentText: `Please enter the city name`
+        })
+        return
     }
-}
 
-function weather(req, res) {
-    var city_Name = req.body.queryResult.outputcontext.parameters.city
-    var url = `https://api.openweathermap.org/data/2.5/weather?q=${city_Name}&units=metric&appid=${apiKey}`
-    rp.get(url, function (err, _res, body) {
+    var session = req.body.session
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`
+    await rq(url, function (err, _res, body) {
         let weather = JSON.parse(body);
+        console.log("weather is: ", weather)
         if (err) {
-            console.log('error:', err);
+            console.log('error is:', err);
             res.send({
                 fulfillmentText: `error while calling api`
             })
         } else {
             res.send({
-                fulfillmentText: `Weather is ${weather}`
+                outputContexts: [
+                    {
+                        "name": `${session}/contexts/abc`,
+                        "lifespanCount": 5,
+                        "parameters": {
+                            "abccity": cityName
+                        }
+                    }
+                ],
+                fulfillmentText: `The rain in ${cityName} ${(weather.rain)?"is"+weather.rain["1h"]+"mm":" is clear"} !`
+
             })
-            console.log(body.weather)
+            return
         }
     })
-    return
+}
+
+
+async function temp(req, res) {
+    console.log("context are: ",req.body.queryResult.outputContexts)
+    var cityName;
+    if (req.body.queryResult.parameters.city) {
+        cityName = req.body.queryResult.parameters.city
+    }
+    else if (req.body.queryResult.outputContexts[1].parameters.abccity) {
+        cityName = req.body.queryResult.outputContexts[1].parameters.abccity
+    }
+    else {
+        res.send({
+            fulfillmentText: `Please enter the city name`
+        })
+        return
+    }
+    var session = req.body.session
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`
+    await rq(url, function (err, _res, body) {
+        let weather = JSON.parse(body);
+        console.log("weather is: ", weather)
+        if (err) {
+            console.log('error is:', err);
+            res.send({
+                fulfillmentText: `error while calling api`
+            })
+        } else {
+            res.send({
+                outputContexts: [
+                    {
+                        "name": `${session}/contexts/abc`,
+                        "lifespanCount": 5,
+                        "parameters": {
+                            "abccity": cityName
+                        }
+                    }
+                ],
+                fulfillmentText: `The temperature in ${cityName} is ${weather.main.temp}°C !`
+
+            })
+            return
+        }
+    })
+}
+
+
+async function weather(req, res) {
+    console.log("context are: ",req.body.queryResult.outputContexts)
+    var cityName;
+    if (req.body.queryResult.parameters.city) {
+        cityName = req.body.queryResult.parameters.city
+    }
+    else if (req.body.queryResult.outputContexts[1].parameters.abccity) {
+        cityName = req.body.queryResult.outputContexts[1].parameters.abccity
+    }
+    else {
+        res.send({
+            fulfillmentText: `Please enter the city name`
+        })
+        return
+    }
+    var session = req.body.session
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`
+    await rq(url, function (err, _res, body) {
+        let weather = JSON.parse(body);
+        console.log("weather is: ", weather)
+        if (err) {
+            console.log('error is:', err);
+            res.send({
+                fulfillmentText: `error while calling api`
+            })
+        } else {
+            res.send({
+                outputContexts: [
+                    {
+                        "name": `${session}/contexts/abc`,
+                        "lifespanCount": 5,
+                        "parameters": {
+                            "abccity": cityName
+                        }
+                    }
+                ],
+                fulfillmentText: `The weather in ${cityName} is ${weather.main} and ${weather.description} !`
+
+            })
+            return
+        }
+    })
 }
 
 app.listen(process.env.PORT || 8088, function () {
     console.log("server is running")
 })
-var a =
-{
-    "outputContexts": [
-        {
-            "name": "projects/weather-bot-c8880/agent/sessions/619c2437-5e4b-13bc-a595-4c735d2d8a6c/context/memory",
-            "lifespanCount": 5,
-            "parameters": {
-                "city": "Hyderabad"
-            }
-        }
-    ],
-    "fulfillmentText": "The humidity in Hyderabad is 60% !"
-}
