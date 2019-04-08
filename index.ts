2b919ffd4f35513a23e379cc40b1b1d734ab1907
Jawwad Turabi <memonjawwad9@gmail.com>
2019-04-08 18:17:35 +0500

    third

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ process.env.DEBUG = "dialogflow:debug"
 app.post("/webhook", function (request, response, next) {
 
     var intent = request.body.queryResult.intent.displayName
-    var cityName = request.body.queryResult.parmeters.city
+    var cityName = request.body.queryResult.parmeters.geo-city
     switch (intent) {
         case 'Default Welcome Intent':
 
